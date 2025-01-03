c68b86cf31e74448ff8b1988d72d5ac49b988080
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2025-01-03 23:25:45 +0300

    implemented CRUD functionality

--- server.js
+++ server.js
@@ -1,51 +1,63 @@
-const http = require('http');
-const Koa = require('koa');
-const { koaBody } = require('koa-body');
-const cors = require('koa-cors');
+const { error } = require("console");
+const http = require("http");
+const Koa = require("koa");
+const { koaBody } = require("koa-body");
+const cors = require("koa-cors");
+const moment = require("moment");
+require("moment/locale/ru");
 const app = new Koa();
 
 const tickets = [
-    {
-        id: 1,
-        name: "Поменять краску в принтере, ком. 404",
-        description: "Принтер HP LJ 1210, картридж на складе",
-        status: "false",
-        created: new Date(),
-    },
-    {
-        id: 2,
-        name: "Переустановить Windows, ПК-Hall24",
-        description: "Переустановить Windows, ключ на почте",
-        status: "true",
-        created: new Date(2024, 11, 21, 15, 11),
-    }
-]
+  {
+    id: 1,
+    name: "Поменять краску в принтере, ком. 404",
+    description: "Принтер HP LJ 1210, картридж на складе",
+    status: "false",
+    created: new Date(),
+  },
+  {
+    id: 2,
+    name: "Переустановить Windows, ПК-Hall24",
+    description: "Переустановить Windows, ключ на почте",
+    status: "true",
+    created: new Date(2024, 11, 21, 15, 11),
+  },
+];
 
-app.use(cors({
-  origin: "*",
-  credentials: true,
-  allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
-}))
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
-app.use(koaBody({
-  text: true,
-  urlencoded: true,
-  multipart: true,
-  json: true,
-}))
+app.use(
+  koaBody({
+    text: true,
+    urlencoded: true,
+    multipart: true,
+    json: true,
+  })
+);
 
-app.use(ctx => {
+app.use((ctx) => {
   ctx.response.set({
     "Access-Control-Allow-Origin": "*",
   });
   const { method } = ctx.request.query;
   switch (method) {
     case "allTickets":
-      ctx.response.body = tickets.map(({description, ...ticket}) => (ticket));
+      ctx.response.body = tickets.map((ticket) => ({
+        ...ticket,
+        created: moment(ticket.created).format("D MMMM YYYY, HH:mm"),
+      }));
       return;
     case "ticketById":
       const { id: ticketId } = ctx.request.query;
-      const ticketShow = tickets.find(ticket => ticket.id === parseInt(ticketId));
+      const ticketShow = tickets.find(
+        (ticket) => ticket.id === parseInt(ticketId)
+      );
       if (ticketShow) {
         ctx.response.body = ticketShow.description;
       } else {
@@ -58,46 +70,86 @@ app.use(ctx => {
         return;
       }
       const data = ctx.request.body;
-
       const ticket = {
-        id: (data.id) ? parseInt(data.id) : (tickets[tickets.length - 1]).id + 1,
+        id: tickets.length ? tickets[tickets.length - 1].id + 1 : 1,
         name: data.name,
         description: data.description,
-        status: (data.status) ? data.status : "false",
+        status: data.status ? data.status : "false",
         created: new Date(),
       };
 
-      if (data.id) {
-        let indexTicket = tickets.findIndex(ticket => ticket.id === parseInt(data.id));
-        tickets.splice(indexTicket, 1, ticket);
-      } else {
-        tickets.push(ticket);
-      }
+      tickets.push(ticket);
 
       ctx.response.status = 201;
-      ctx.response.body = tickets;
-
+      ctx.response.body = tickets.map((ticket) => ({
+        ...ticket,
+        created: moment(ticket.created).format("D MMMM YYYY, HH:mm"),
+      }));
       return;
 
-    case "deleteTicket": 
-    if (ctx.request.method !== "POST") {
+    case "deleteTicket":
+      if (ctx.request.method !== "POST") {
+        ctx.response.status = 404;
+        return;
+      }
+      const { id: ticketToDelete } = ctx.request.body;
+      const index = tickets.findIndex(
+        (ticket) => ticket.id === parseInt(ticketToDelete)
+      );
+      if (index === -1) {
+        ctx.response.status = 404;
+        return;
+      }
+      tickets.splice(index, 1);
+      ctx.response.body = tickets.map((ticket) => ({
+        ...ticket,
+        created: moment(ticket.created).format("D MMMM YYYY, HH:mm"), // Форматируем дату
+      }));
+      return;
+    default:
+      server.js;
       ctx.response.status = 404;
       return;
-    }
-    const { id: ticketToDelete} = ctx.request.query;
-    const index = tickets.findIndex(ticket => ticket.id === parseInt(ticketToDelete));
-    if (index === -1) {
-      ctx.response.status = 404;
+    case "editTicket":
+      if (ctx.request.method !== "POST") {
+        ctx.response.status = 404;
+        return;
+      }
+
+      const {
+        id: ticketIdToEdit,
+        name,
+        description,
+        status,
+      } = ctx.request.body;
+
+      const ticketIndex = tickets.findIndex(
+        (ticket) => ticket.id === parseInt(ticketIdToEdit)
+      );
+
+      if (ticketIndex === -1) {
+        ctx.response.status = 404;
+        ctx.response.body = { error: "Тикет не найден" };
+        return;
+      }
+
+      const updateTicket = {
+        ...tickets[ticketIndex],
+        name: name || tickets[ticketIndex].name,
+        description: description || tickets[ticketIndex].description,
+        status: status || tickets[ticketIndex].status,
+      };
+
+      tickets.splice(ticketIndex, 1, updateTicket);
+
+      ctx.response.status = 200;
+      ctx.response.body = tickets.map((ticket) => ({
+        ...ticket,
+        created: moment(ticket.created).format("D MMMM YYYY, HH:mm"), // Форматируем дату
+      }));
       return;
-    }
-    tickets.splice(index, 1);
-    ctx.response.body = tickets;
-    return;
-    default: server.js
-    ctx.response.status = 404;
-    return;
   }
-})
+});
 
 const port = process.env.PORT || 9000;
 const server = http.createServer(app.callback()).listen(port);
